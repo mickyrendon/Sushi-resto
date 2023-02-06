c2baa2df306b212b71cb5f46d7abe67f0fb982a9
micky rendon <elderendon@hotmail.es>
2023-02-06 13:41:32 -0300

    preloader

--- dist/index.js
+++ dist/index.js
@@ -1,27 +1,29 @@
-// import { addImg } from '../src/js/gallery/createCard.js'
+import { deleteLoader } from '../src/js/loader/loader.js'
 import { cardsIterator } from '../src/js/gallery/createCard.js'
 import { mediaQ } from '../src/js/menu/mediaqueries.js'
-// import 'animate.css'
-
-window.onload = () => cardsIterator
-// desktop menu
-mediaQ()
-// menu mobile
-const mobileMenuBtn = document.querySelector('.menu-mobile')
-
-mobileMenuBtn.addEventListener('click', async () => {
-    const { mobileMenu } = await import('../src/js/menu/mobileMenu.js')
-    return mobileMenu()
-})
-
-// download menu
-const downloadMenu = document.querySelector('.download-menu')
-
-downloadMenu.addEventListener('click', async (ev) => {
-    const { downloadPdf } = await import('../src/js/menuDownload/downloadBtn.js')
-    return downloadPdf(ev)
-})
 
 
+globalThis.onload = () => {
 
+    cardsIterator
+    // desktop menu
+    mediaQ()
+    // menu mobile
+    const mobileMenuBtn = document.querySelector('.menu-mobile')
+
+    mobileMenuBtn.addEventListener('click', async () => {
+        const { mobileMenu } = await import('../src/js/menu/mobileMenu.js')
+        return mobileMenu()
+    })
+
+    // download menu
+    const downloadMenu = document.querySelector('.download-menu')
+
+    downloadMenu.addEventListener('click', async (ev) => {
+        const { downloadPdf } = await import('../src/js/menuDownload/downloadBtn.js')
+        return downloadPdf(ev)
+    })
+
+    return deleteLoader()
+}
 
