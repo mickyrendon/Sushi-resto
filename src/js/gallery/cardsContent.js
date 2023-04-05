b64f9b7e28782aac465909e8c068dbb35c6eb51d
micky rendon <elderendon@hotmail.es>
2023-04-05 17:16:18 -0400

    [add] adding new images

--- src/js/gallery/cardsContent.js
+++ src/js/gallery/cardsContent.js
@@ -13,38 +13,39 @@ export const menu = [
         description: 'Rollo de arroz invertido con atún picante, palta y pepino. Decorado con cebollino y semillas de sésamo.',
         price: '$00.00',
         image: {
-            // FIXME, falta imagen
-            src : '../src/assets/images/gallery/tinyfied/03.jpg',
+            src : '../src/assets/images/gallery/tinyfied/uramaki.jpg',
         }
     },
     
     {
+        // FIXME, cambiar por una foto adecuada a las demás
         title: 'Temaki Ebi Crunchy',
         description: 'Cono de alga nori relleno de arroz, langostinos crujientes, pepino y lechuga. Decorado con salsa teriyaki y sésamo tostado.',
         price: '$00.00',
         image: {
-            // FIXME, falta imagen
-            src : '../src/assets/images/gallery/tinyfied/05.jpg',
+            src : '../src/assets/images/gallery/tinyfied/temaki_ebi.jpg',
         }
     },
     {
+        // FIXME, cambiar por una foto adecuada a las demás
         title: 'Gunkan Ikura',
         description: 'Pequeñas naves de arroz coronadas con huevas de salmón y una fina tira de alga nori. Decorado con salsa ponzu y wasabi.',
         price: '$00.00',
         image: {
-            // FIXME, falta imagen
-            src : '../src/assets/images/gallery/tinyfied/06.jpg',
+            src : '../src/assets/images/gallery/tinyfied/gunkan.jpg',
         }
     },
     {
+        // FIXME, cambiar por una foto adecuada a las demás
         title: 'Futomaki',
         description: 'Rollo grande de sushi con diversos ingredientes, como kanikama (carne de cangrejo), tortilla japonesa, pepino y shiitake. Decorado con masago (huevas de pescado volador) y sésamo blanco y negro.',
         price: '$00.00',
         image: {
-            src : '../src/assets/images/gallery/tinyfied/07.jpg',
+            src : '../src/assets/images/gallery/tinyfied/futomaki.png',
         }
     },
     {
+        // FIXME, cambiar por una foto adecuada a las demás
         title: 'Nigiri Sake',
         description: 'Bocado de arroz con salmón fresco, ligeramente ahumado. Decorado con una fina tira de alga nori y wasabi.',
         price: '$00.00',
@@ -57,15 +58,16 @@ export const menu = [
         description: 'Rollo invertido con langostinos tempura, palta y pepino. Decorado con una fina capa de aguacate y salsa teriyaki.',
         price: '$00.00',
         image: {
-            src : '../src/assets/images/gallery/tinyfied/09.jpg',
+            src : '../src/assets/images/gallery/tinyfied/Dragon_Rolls.webp',
         }
     },
     {
+        // FIXME, cambiar por una foto adecuada a las demás
         title: 'Temaki Kani',
         description: 'Cono de alga nori relleno de arroz, kanikama, pepino y cebolla de verdeo. Decorado con salsa mayonesa japonesa y masago.',
         price: '$00.00',
         image: {
-            src: '../src/assets/images/gallery/tinyfied/02.jpg',
+            src: '../src/assets/images/gallery/tinyfied/temaki_kani.jpg',
         }
     },
     {
@@ -73,15 +75,16 @@ export const menu = [
         description: 'Pequeñas naves de arroz con tentáculo de pulpo cocido y una fina tira de alga nori. Decorado con salsa de soja y jengibre encurtido.',
         price: '$00.00',
         image: {
-            src : '../src/assets/images/gallery/tinyfied/11.jpg',
+            src : '../src/assets/images/gallery/tinyfied/gunkan_tako.jpg',
         }
     },
     {
+        // FIXME, cambiar por una foto adecuada a las demás
         title: 'Maki Philadelphia',
         description: 'Rollo de arroz relleno de salmón ahumado, queso crema y pepino. Decorado con sésamo blanco y negro.',
         price: '$00.00',
         image: {
-            src : '',
+            src : '../src/assets/images/gallery/tinyfied/16.jpg',
         }
     },
     {
@@ -93,11 +96,12 @@ export const menu = [
         }
     },
     {
+        // FIXME, cambiar por una foto adecuada a las demás
         title: 'Uramaki Rainbow Roll',
         description: 'Rollo invertido con surimi, aguacate y pepino, coronado con láminas de salmón, atún y palta. Decorado con salsa ponzu y masago.',
         price: '$00.00',
         image: {
-            src : '../src/assets/images/gallery/tinyfied/14.jpg',
+            src : '../src/assets/images/gallery/tinyfied/uramaki_rainbow.jpg',
         }
     },
     {
@@ -105,7 +109,7 @@ export const menu = [
         description: 'Cono de alga nori relleno de arroz, anguila a la parrilla y pepino. Decorado con salsa kabayaki y semillas de sésamo.',
         price: '$00.00',
         image: {
-            src : '../src/assets/images/gallery/tinyfied/15.jpg',
+            src : '../src/assets/images/gallery/tinyfied/temaki_unagi.webp',
         }
     },
     {
@@ -121,7 +125,7 @@ export const menu = [
         description: 'Elaborado con palta, pepino, langostinos, arroz y alga nori. Decorado con sésamo blanco y negro.',
         price: '$00.00',
         image: {
-            src : `../src/assets/images/gallery/tinyfied/16.jpg`
+            src : `../src/assets/images/gallery/tinyfied/15.jpg`
         }
     },
     // {
