a4b951ad418d2ae3b8b2944ded131e8fd7a21657
micky rendon <elderendon@hotmail.es>
2023-04-10 13:16:54 -0400

    [features]

--- src/index.js
+++ src/index.js
@@ -2,6 +2,7 @@ import { deleteLoader } from './js/loader/loader.js'
 import { cardsIterator } from './js/gallery/cards/createCard.js'
 import { mediaQ } from './js/menu/desktop/mediaqueries.js'
 import { swiper } from './js/slider/swiper.js'
+import { mobileMenu } from './js/menu/mobile/mobileMenu.js'
 // import "../src/css/vars.css"
 // import '../src/css/styles.css'
 // import '../node_modules/animate.css/animate.min.css'
@@ -13,29 +14,34 @@ cardsIterator
 mediaQ()
 // menu mobile
 const mobileMenuBtn = document.querySelector('.menu-mobile')
-
-mobileMenuBtn.addEventListener('click', async () => {
-    const { mobileMenu } = await import('./js/menu/mobile/mobileMenu.js')
-    return mobileMenu()
-})
+      mobileMenuBtn.addEventListener('click', mobileMenu, true)
 
 
 
 const deliverBtn = document.querySelector('body')
-deliverBtn.addEventListener('click', async (e) => {
-    const { deliverList } = await import('./js/deliverList/deliverListNode.js')
-    // e.preventDefault()
+// FIXME, con doble click se duplica la imagen
+deliverBtn.addEventListener('onclick', async (e) => {
+    e.preventDefault()
+    e.stopImmediatePropagation()
     if(e.target.title === 'deliver-btn'){
-        console.log('deliver btn main');
+        const { deliverList } = await import('./js/deliverList/deliverListNode.js')
         return deliverList()
     }else if(e.target.title === 'deliver-btn-nav'){
-        console.log('deliver btn nav');
+        const { deliverList } = await import('./js/deliverList/deliverListNode.js')
         return deliverList()
     }
-    
+
 })
 
 
 globalThis.addEventListener('load', () => {
     return deleteLoader() 
 })
+
+
+// 
+/* mobileMenuBtn.addEventListener('click', async () => {
+    const { mobileMenu } = await import('./js/menu/mobile/mobileMenu.js')
+    return mobileMenu()
+}) 
+*/
